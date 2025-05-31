74b78db2025b9a00379a6b823f7fb2540cb2a314
PSYEONE <chrizioannou@gmail.com>
2025-05-31 18:13:31 +0100

    vercel intergration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
+  config: {
+    turbopack: {
       rules: {
         '*.mp4': {
           loaders: ['file-loader'],
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { React, Suspense } from 'react';
+import { list } from '@vercel/blob';
 
 const Title = () => {
   return (
@@ -10,11 +11,24 @@ const Title = () => {
 
 const Background = () => {
   return (
-    <div>
-      <video autoPlay loop muted>
-        <source src={require('./media/girls last tour/ep1.mp4')} type='video/mp4' />
-     </video>
-    </div>
+    <Suspense fallback={<p>Loading...</p>}>
+      <VideoComponent fileName="ep1.mp4" />
+    </Suspense>
+  )
+}
+
+async function VideoComponent({ fileName}) {
+  const { blobs } = await list({
+    prefix: fileName,
+    limit: 1,
+  })
+  const { url } = blobs[0]
+
+  return (
+    <video autoPlay muted loop preload="none">
+      <source src={url} type="video/mp4" />
+      Your browser doesn't support video tag.
+    </video>
   )
 }
 
